29e8b535bfc42176f72183945ec3b147b6964b93
Wajdi Htiouech <97262256+wajdiht@users.noreply.github.com>
2024-11-10 18:43:01 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,13 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-import { BrowserRouter } from 'react-router-dom'  // Import BrowserRouter for routing
-
-// GitHub Pages deployment: set basename to the repo name
-const basename = '/<e-plantShopping>';  // Replace <repository-name> with your repo's name
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* Wrap the App with BrowserRouter and set basename */}
-      <BrowserRouter basename={basename}>
-        <App />
-      </BrowserRouter>
+    <App />
     </Provider>
   </React.StrictMode>,
 )
