1061fc0e109bbb0e8bc621220f064b14ab7a1a10
Wajdi Htiouech <97262256+wajdiht@users.noreply.github.com>
2024-11-10 19:39:16 +0100

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,36 +4,51 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
-    TotalCartItems:0
   },
   reducers: {
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;
       const existingItem = state.items.find(item => item.name === name);
-      state.TotalCartItems++;
       if (existingItem) {
         existingItem.quantity++;
-        
       } else {
         state.items.push({ name, image, cost, quantity: 1 });
       }
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action.payload.name);
-      state.TotalCartItems = state.TotalCartItems- action.payload.quantity;
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        state.items = state.items.filter(item => item.name !== name);
+      } else {
+        console.log("Something weird! Didn't find the item in the cart.");
+      }
     },
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
+      const { item, change } = action.payload;
+      console.log(item);
+      console.log(change);
+      const name = item.name;
       const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity - 1;
+      if (change === 'increment') {
+        if (itemToUpdate) {
+          itemToUpdate.quantity = itemToUpdate.quantity + 1;
+        } else{
+          console.log("Something weird! Didn't find the item in the cart.");
+        }
+      } else if (change === 'decrement') {
+        if (itemToUpdate) {
+          itemToUpdate.quantity = itemToUpdate.quantity - 1;
+          if (itemToUpdate.quantity === 0) {
+            state.items = state.items.filter(item => item.name !== name);
+          }
+        } else{
+          console.log("Something weird! Didn't find the item in the cart.");
+        }
       }
-      state.TotalCartItems--;
-    
     },
   },
 });
-
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
